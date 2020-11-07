3ee41b6367c577cb1f6327f4c1aefa61c305cb61
Felipe <bd.bumdigital@gmail.com>
2020-11-07 18:07:20 -0300

    Atualização2

--- menu.php
+++
@@ -1,9 +0,0 @@
-<!--Início do Menu-->
-    <nav class="navbar navbar-dark bg-success px-5">
-        <a href="index.php"><img width="100px" src="imagens\logo.png"></a>
-        <a class="text-light" href="produtos.php">Amigões de estimação</a>
-        <a class="text-light" href="loja.php">Nossas lojas</a>
-        <a class="text-light" href="pedidos.php">Pedidos</a>
-        <a class="text-light" href="contato.php">Contato</a>
-    </nav>
-<!--Fim do Menu-->
--- rodape.php
+++
@@ -1,8 +0,0 @@
-<footer id="rodape">
-        <p id="formas_pagamento" class="text-success"><strong>Formas de pagamento</strong></p>
-        <img src="imagens\formasdepagamento.png" alt="Formas de pagamento">
-        <p>&copy; Amigão Pet</p>
-    </footer>
-</body>
-
-</html>
